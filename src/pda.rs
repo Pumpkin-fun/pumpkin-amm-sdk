8f8c86ae2fbcc53824f964b5d4e8dd6d193e0b91
0xdevissac <issac@pumpkin.fun>
2025-03-25 01:49:01 +0500

    fix pda

--- src/pda.rs
+++ src/pda.rs
@@ -1,7 +1,7 @@
 use anchor_lang::solana_program::pubkey::Pubkey;
 
 pub fn get_global_config_address(bounding_curve_program: &Pubkey) -> Pubkey {
-    Pubkey::find_program_address(&[b"bonding_curve"], bounding_curve_program).0
+    Pubkey::find_program_address(&[b"global_config"], bounding_curve_program).0
 }
 
 pub fn get_sol_vault_address(bounding_curve_program: &Pubkey) -> Pubkey {
